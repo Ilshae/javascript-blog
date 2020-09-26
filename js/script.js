e1488ea8d0337f1b6d2a40a354b48ee7a904f3a0
Ingrid Pruszynska <pruszynskaingrid@outlook.com>
2020-09-26 13:16:04 +0200

    Get href from clicked link, find correct article and add active class

--- js/script.js
+++ js/script.js
@@ -3,29 +3,35 @@
 function titleClickHandler(event){
     console.log(event);
 
-  /* [DONE] remove class 'active' from all article links  */
+  /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
 
-  /* [DONE] add class 'active' to the clicked link */
+  /* add class 'active' to the clicked link */
+    event.preventDefault();
     const clickedElement = this;
     clickedElement.classList.add('active');  
     console.log('clickedElement:', clickedElement);
 
-  /* [DONE] remove class 'active' from all articles */
+  /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
 
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
   /* get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute("href");
+    console.log("articleSelector:", articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log("targetArticle:", targetArticle);
 
   /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
